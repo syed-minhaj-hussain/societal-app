743727f2bf508889c33e7c1f4f970da4f4d9d08d
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-09-09 20:23:12 +0530

    refactor

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import searchStyle from "./search.module.css";
+import userStyle from "../../features/user/user.module.css";
 import { searchUserByName } from "../../features/user/userSlice";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Image } from "cloudinary-react";
 
 export const Search = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -31,16 +34,57 @@ export const Search = () => {
   console.log({ searchStatus });
   return (
     <div className={searchStyle.main}>
-      <form>
-        <label htmlFor="search">Search User</label>
-        <br />
+      <form
+        className={searchStyle.form}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {/*<label
+          htmlFor="search"
+          className={searchStyle.label}
+          style={{ textAlign: "center" }}
+        >
+          Search User
+        </label>
+        <br /> */}
         <input
           type="text"
           id="search"
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
+          className={searchStyle.input}
+          placeholder="Search User.."
         />
       </form>
+      {users?.map((user) => (
+        <div className={searchStyle.searchComponent} key={user?._id}>
+          <Link
+            to={`/user/${user?._id}`}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <div className={userStyle.postUser}>
+              {user?.profilePicture === "https://i.ibb.co/RztnCHv/user.png" ? (
+                <img
+                  src="https://i.ibb.co/RztnCHv/user.png"
+                  alt=""
+                  className={userStyle.postProfile}
+                />
+              ) : (
+                <Image
+                  cloudName="dtb0aupd7"
+                  publicId={user?.profilePicture}
+                  className={userStyle.postProfile}
+                />
+              )}
+              <span style={{ padding: "1rem" }}>
+                <span className={userStyle.postName}>{user?.name}</span>
+                <span className={userStyle.postUsername}>{user?.username}</span>
+              </span>
+            </div>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
